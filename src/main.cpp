8ac6ff68fee0fe8b79097bbaeb0c9467b165988e
serj <serj.dub1na@gmail.com>
2018-08-26 22:24:29 +0300

    Edit heating time structure

--- src/main.cpp
+++ src/main.cpp
@@ -5,56 +5,32 @@
     Created:	17.07.2018 20:15:56
     Author:     Serj
 */
-
-#include <Arduino.h>
-#include <Wire.h>
-#include <RTClib.h>
-#include <avr/eeprom.h>
-
-#define TRIAC_PIN 9
+#include "main.h"
 
 DS3231 rtc;
 
-enum DAYS
-{
-	MONDAY,
-	TUESDAY,
-	WEDNESDAY,
-	THURSDAY,
-	FRIDAY,
-	SATURDAY,
-	SUNDAY
-};
-
-/* Примерно так хранить в EEPROM */
-enum EEPROM_DATE_STORAGE
-{
-	DAY,
-	HOURS1,
-	MINUTES1,
-	HOURS2,
-	MINUTES2
-};
-
-uint8_t frame[5] = { MONDAY, 23, 0, 7, 0 };
-//DAYS temp = frame[DAY];
-/********************************************/
-
-struct heatTimes
-{
-	DAYS day;
-	uint8_t time[4];
-};
-
+/* 
 heatTimes heatTime[7] =
 {
-	{.day = MONDAY,    .time = { 1,  2,  3 }},
+	{.day = MONDAY,    .time = { 2030,  2,  3 }},
 	{.day = TUESDAY,   .time = { 4,  5,  6 }},
 	{.day = WEDNESDAY, .time = { 7,  8,  9 }},
 	{.day = THURSDAY,  .time = { 10, 11, 12 }},
 	{.day = FRIDAY,    .time = { 13, 14, 15 }},
 	{.day = SATURDAY,  .time = { 16, 17, 18 }},
 	{.day = SUNDAY,    .time = { 19, 20, 21 }},
+};
+ */
+
+heatTimes heatTime[7] =
+{
+	{.day = MONDAY,    .time[0] = {20, 30} },
+	{.day = TUESDAY,   },
+	{.day = WEDNESDAY, },
+	{.day = THURSDAY,  },
+	{.day = FRIDAY,    },
+	{.day = SATURDAY,  },
+	{.day = SUNDAY,    },
 };
 
 //uint8_t times[] = { 2307, 2307, 2307, 2307, 2307, 2307, 2307 };
@@ -67,6 +43,7 @@ void setup()
 	Wire.begin();
 	rtc.begin();
 }
+
 
 void loop()
 {
@@ -95,4 +72,9 @@ void loop()
 		digitalWrite(LED_BUILTIN, LOW);
 	
 	*/
+}
+
+bool needHeat()
+{
+
 }
